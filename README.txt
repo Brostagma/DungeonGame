8d1b89f78ba0a0ac1a9dc88c3caae7d65c939b5f
AhmetEmin <eminberoglu@gmail.com>
2022-04-24 09:21:35 +0300

    What we do now?

---
+++ README.txt
@@ -0,0 +1,4 @@
+24 April 09:15:16
+Brostagma (Ahmet Emin)
+
+This repository is for leraning object oriented programming (C++)
---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+/*
+ * main.cpp
+ *
+ *  Created on: Apr 24, 2022
+ *      Author: brostagma(Ahmet Emin)
+ */
+
+//What we do now ?
+
+
+
